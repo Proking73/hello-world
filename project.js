8f8ec8d819525aa96ce14bafdb56b2d876c47022
Proking73 <77035338+Proking73@users.noreply.github.com>
2021-09-14 18:54:51 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,5 +1,5 @@
 //https://proking73.github.io/hello-world/
 document.getElementById("JELLLO_BELLO").innerHTML = "BELLO_JELLLO";
 // using " " is do add a space in between words, for example 'let yeeeet_car = 1 + "2"'
-let yeeeet_car = 1 + "2"
-prin(yeeeet_car);
+let yeeeet_car = 1 + "2";
+print(yeeeet_car);
