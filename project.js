f6899efe77eab6bbc5a1d7cfc22fce1f75254d8a
77035338 <MY_NAME@example.com>
2021-11-06 20:43:18 -0400

    aave

--- project.js
+++ project.js
@@ -3,8 +3,13 @@
 // using " " is do add a space in between words, for example 'let yeeeet_car = 1 + "2"'
 let yeeeet_car = 1 + "2";
 text("yeeeet_car");*/
-var file = document.getElementById("fiel").innerHTML;
-document.addEventListener("click", function() {
-    window.alert(file.value);
-})
-
+//var file = document.getElementById("fiel").innerHTML;
+//document.addEventListener("click", function() {
+//    window.alert(file.value);
+//})
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  var insept = document.getElementById("inputss").value;
+  console.log(insept);
+  document.getElementById("insept").innerHTML = toCelsius(insept);
